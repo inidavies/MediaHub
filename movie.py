24b80b1715f79391d0ee171a817a511812625897
Kyle Olivier <kyleolivier12@gmail.com>
2022-07-27 20:15:29 +0000

    Added correct video output

--- movie.py
+++ movie.py
@@ -2,24 +2,27 @@ import requests
 import json
 import pandas as pd
 import sqlalchemy as db
-import urllib.request
-import re
 
 col_names = ['Title', 'Release Date', 'Trailer']
 df = pd.DataFrame(columns=col_names)
 
 def movie_search(movie_input):
         i = 0
+        youtube = 'https://www.youtube.com/watch?v='
         request = requests.get('https://api.themoviedb.org/3/search/movie?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(movie_input)).json()
-        movie_input = movie_input.replace(' ', '+')
-        html = urllib.request.urlopen('https://www.youtube.com/results?search_query=' + movie_input)
-        video_ids = re.findall(r'watch\?v=(\S{11})', html.read().decode())
         try:
             while i < len(request['results']):
-                html = urllib.request.urlopen('https://www.youtube.com/results?search_query=' + request['results'][i]['original_title'])
-                video_ids = re.findall(r'watch\?v=(\S{11})', html.read().decode())
+                vid_request = requests.get('https://api.themoviedb.org/3/movie/{}/videos?active_nav_item=Trailers&api_key=ca247a677e49a57329443e35dfc24974&language=en-US'.format(request['results'][i]['id'])).json()
                 movie_id = request['results'][i]['original_title']
-                df.loc[len(df.index)] = [request['results'][i]['original_title'], request['results'][i]['release_date'], 'https://www.youtube.com/watch?v=' + video_ids[0]]
+                j = 0
+                key = ''
+                while j < len(vid_request['results']):
+                    movie_video = vid_request['results'][j]['type']
+                    if movie_video == 'Trailer':
+                        key = vid_request['results'][j]['key']
+                        break
+                    j += 1
+                df.loc[len(df.index)] = [request['results'][i]['original_title'], request['results'][i]['release_date'], youtube + key]
                 i += 1
         except IndexError:
             print('No movies for this search')
