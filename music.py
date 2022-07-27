0d3c99bcf5b264c1f0ba2529358b43b263c4ab5d
Auburn Robinson <alrobinson4@dons.usfca.edu>
2022-07-27 20:34:49 +0000

    returns artist yt song link and song title

--- music.py
+++ music.py
@@ -1,8 +1,14 @@
 from ytmusicapi import YTMusic
-import Flask
+# import Flask
 
 
-ytmusic = YTMusic()
+def music():
+    ytmusic = YTMusic()
+    search_results = ytmusic.search("Suge" , "songs")
+    songName = search_results[0]['title']
+    mainArtist = search_results[0]['artists'][0]['name']
+    videoLink = "https://www.youtube.com/watch?v=" + str(search_results[0]['videoId'])
 
-search_results = ytmusic.search("Suge" , "songs")
-print("https://www.youtube.com/embed/" + str(search_results[0]['videoId']))
+    return songName, mainArtist, videoLink
+
+music()
