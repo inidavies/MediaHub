b731cf4a524d2c5774192bc6ea410d3214c99726
Mark Karomo <mk772@cairn.edu>
2022-07-28 17:10:12 +0000

    working with Ini version

--- books.py
+++ books.py
@@ -10,22 +10,40 @@ from pprint import pprint
 search = input("Please enter a book title ?")
 #defining a function to access the api and send a get request
 def find_books():
-        url = f'https://www.googleapis.com/books/v1/volumes?q={"search"}&key=AIzaSyD_3CEF5P_6azVom7nW_Hs-ECYrXSAbm_M'
+        url = f'https://www.googleapis.com/books/v1/volumes?q={search}&key=AIzaSyD_3CEF5P_6azVom7nW_Hs-ECYrXSAbm_M'
         #send request to API
         response = requests.get(url)
         #parse json into python as dictionary 
         book_data = response.json()
+        pprint(book_data['items'])
+        '''
+        book_info = book_data["items"]
+        #author = book_info["authors"]
+        data_container = []
+        for book in book_info:
+                book_title = book["volumeInfo"]["title"]
+                book_author = book["volumeInfo"]["authors"]
+                book_purchase = book["volumeInfo"]["buyLink"] 
+                book_coverphoto = book["volumeInfo"]["thumbnail"]
+                current_book = {"name":book_title, "author":book_author, "link":book_purchase, "image":book_coverphoto }
+                print(current_book)
+                print()
 
-        book_info = book_data["items"][0]["volumeInfo"]
-        author = book_info["authors"]
+                
+                
 
-        print(f"\nTitle: {book_info['title']}")
-        print(f"Author: {author}")
+
+
+
+
+
+        #print(f"\nTitle: {book_info['title']}")
+        #print(f"Author: {author}")
         #print(f"\nBuyLink: {book_info['buyLink']}")
         #print(f"\ncoverPhoto: {book_info['thumbnail']}")
 
         #print(data['items']['volumeInfo']['title'])
 
        
-
+'''
 find_books()                    
