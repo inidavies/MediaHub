6db0a34c81fdb0d3fb634cd51d588b475bc03363
Inioluwa Davies <71574716+inidavies@users.noreply.github.com>
2022-07-28 13:39:25 -0500

    Update movie.py
    Adjusted for front-end

--- movie.py
+++ movie.py
@@ -2,19 +2,37 @@ import requests
 import json
 import pandas as pd
 import sqlalchemy as db
+import os
 
 #  Dataframe
 col_names = ['Title', 'Release Date', 'Trailer']
 df = pd.DataFrame(columns=col_names)
 
-def movie_search(movie_input):
+MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+TITLE_URL_A = "https://api.themoviedb.org/3/search/movie?"
+TITLE_URL_B = f'api_key={MOVIE_API_KEY}'
+TITLE_URL_C = "&language=en-US&page=1&"
+
+TRAILER_URL_A = "https://api.themoviedb.org/3/"
+TRAILER_URL_C = f'api_key={MOVIE_API_KEY}&language=en-US'
+
+# defining import * for the __init__.py file (to obscure other functions)
+__all__ = ['get_movies']
+
+def get_movies(movie_input):
+        TITLE_URL_D = f"query={movie_input}&include_adult=false"
+        data_container = []
         i = 0
         youtube = 'https://www.youtube.com/watch?v='
-        request = requests.get('https://api.themoviedb.org/3/search/movie?api_key=ca247a677e49a57329443e35dfc24974&language=en-US&page=1&query={}&include_adult=false'.format(movie_input)).json()
+        data = requests.get(TITLE_URL_A + TITLE_URL_B + TITLE_URL_C + TITLE_URL_D)
+        request = data.json()
         try:
             #  To get the movie titles
             while i < len(request['results']):
-                vid_request = requests.get('https://api.themoviedb.org/3/movie/{}/videos?active_nav_item=Trailers&api_key=ca247a677e49a57329443e35dfc24974&language=en-US'.format(request['results'][i]['id'])).json()
+                current_id = request['results'][i]['id']
+                TRAILER_URL_B = f"movie/{current_id}/videos?active_nav_item=Trailers&"
+                data = requests.get(TRAILER_URL_A + TRAILER_URL_B + TRAILER_URL_C)
+                vid_request = data.json()
                 movie_id = request['results'][i]['original_title']
                 j = 0
                 key = ''
@@ -27,22 +45,14 @@ def movie_search(movie_input):
                     j += 1
                 #  To catch movies without trailers and put into dataframe
                 if len(key) >= 1:
-                    df.loc[len(df.index)] = [request['results'][i]['original_title'], request['results'][i]['release_date'], youtube + key]
-                else:
-                    no_key = 'No trailer for this movie'
-                    df.loc[len(df.index)] = [request['results'][i]['original_title'], request['results'][i]['release_date'], no_key]
+                    movie_name = request['results'][i]['original_title']
+                    movie_date = request['results'][i]['release_date']
+                    movie_link = youtube + key
+                    df.loc[len(df.index)] = [movie_name, movie_date, movie_link]
+                    current_movie = {"name": movie_name, "date": movie_date, "link": movie_link}
+                    data_container.append(current_movie)
                 i += 1
         except IndexError:
-            print('No movies for this search')
-
-
-def main():
-    movie_input = str(input('Search for movie or "no" to exit when prompted: '))
-    while movie_input != ('no' or 'No'):
-        movie_search(movie_input)
-        print('Search for another movie?')
-        movie_input = str(input())
-    for i in range(len(df)):
-        print(df.loc[i, :].to_string())
-if __name__ == '__main__':
-    main()
+            return -1
+        
+        return data_container
