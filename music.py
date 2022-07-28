73a2d6d8c8084b15b995aff23133558e9455e52a
Auburn Robinson <alrobinson4@dons.usfca.edu>
2022-07-28 00:41:49 +0000

    finished music

--- music.py
+++ music.py
@@ -3,12 +3,14 @@ from ytmusicapi import YTMusic
 
 
 def music():
+    song = input(" what song would you like?: ")
     ytmusic = YTMusic()
-    search_results = ytmusic.search("Suge" , "songs")
+    search_results = ytmusic.search(song , "songs")
     songName = search_results[0]['title']
     mainArtist = search_results[0]['artists'][0]['name']
     videoLink = "https://www.youtube.com/watch?v=" + str(search_results[0]['videoId'])
+    thumbnail = search_results[0]['thumbnails'][0]['url']
 
-    return songName, mainArtist, videoLink
+    return songName, mainArtist, videoLink, thumbnail
 
-music()
+print(music())
