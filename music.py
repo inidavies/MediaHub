57d8279e91cff2492e0708349d454ba347881d0c
Aubrey Robinson <71529623+AubreezyR@users.noreply.github.com>
2022-07-29 19:39:18 -0700

    adding random music feature

--- music.py
+++ music.py
@@ -1,9 +1,11 @@
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+from pprint import pprint
 from ytmusicapi import YTMusic
-# import Flask
+import random
 
 
-def music():
-    song = input(" what song would you like?: ")
+def music(song):
     ytmusic = YTMusic()
     search_results = ytmusic.search(song , "songs")
     songName = search_results[0]['title']
@@ -13,4 +15,17 @@ def music():
 
     return songName, mainArtist, videoLink, thumbnail
 
-print(music())
+def randomMusic():
+    rand = random.randint(0, 49)
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+    pl_id = 'spotify:playlist:37i9dQZF1DXcBWIGoYBM5M'
+
+    listOfTopHits = []
+
+    response = sp.playlist_items(pl_id)
+    for i in range(len(response['items'])):
+        listOfTopHits.append(response['items'][i]['track']['name'])
+    print(music(listOfTopHits[rand]))
+    
+randomMusic()
